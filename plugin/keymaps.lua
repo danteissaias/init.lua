f4c7428dc94f8c0f7df1cf306e67f6efdf366627
Dante Issaias <dante@issaias.com>
2024-11-04 23:10:32 +0200

    Cleanup keybinds

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,16 +1,13 @@
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+local set = vim.keymap.set
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = "Hover diagnostics" })
+set("n", "<leader>vd", vim.diagnostic.open_float)
 
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+set("n", "<c-j>", "<c-w><c-j>")
+set("n", "<c-k>", "<c-w><c-k>")
+set("n", "<c-l>", "<c-w><c-l>")
+set("n", "<c-h>", "<c-w><c-h>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
