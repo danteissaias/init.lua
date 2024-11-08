fd20d32651497c66651e6304628d2f40daeab24f
Dante Issaias <dante@issaias.com>
2024-11-08 14:12:48 +0000

    Keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -9,6 +9,9 @@ set("n", "<c-k>", "<c-w><c-k>")
 set("n", "<c-l>", "<c-w><c-l>")
 set("n", "<c-h>", "<c-w><c-h>")
 
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
